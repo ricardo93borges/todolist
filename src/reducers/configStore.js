a47af96506a83972bab4105d91cfe376a206713c
Ricardo Borges <ricardo93borges@gmail.com>
2019-04-02 11:33:54 -0300

    Using redux dev tools

--- src/reducers/configStore.js
+++ src/reducers/configStore.js
@@ -1,18 +1,10 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './index'
 
 export default ({ initialState } = {}) => {
-    const store = createStore(rootReducer, initialState, applyMiddleware(logger, thunk))
+    const enhancer = compose(applyMiddleware(thunk), logger())
+    const store = createStore(rootReducer, initialState, enhancer)
     return store
 }
-
-const logger = ({dispatch, getState}) => (next) => (action) => {
-    console.group(`LOGGER->${action.type}`)
-    console.log('Will dispatch: ', action)
-    console.log('state: ', getState())
-    const nextAction = next(action)
-    console.log('next state: ', getState())
-    console.groupEnd(`LOGGER->${action.type}`)
-    return nextAction
-}
+const logger = () => window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (x) => x
